313502133c41d5872fee9628137293e991d4a94e
LaurynasM <laurynas.minelga@hostinger.com>
2021-08-11 00:54:34 +0300

    Bot: Index hotfix

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const client = new discord.Client({ disableMentions: 'everyone' });
 const { Player } = require('discord-player');
 
 client.player = new Player(client);
-client.config = require('./config/bot');
+client.config = require(__dirname+'/config/bot');
 client.emotes = client.config.emojis;
 client.filters = client.config.filters;
 client.commands = new discord.Collection();
 
-fs.readdirSync('./commands').forEach(dirs => {
-    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+fs.readdirSync(__dirname+'/commands').forEach(dirs => {
+    const commands = fs.readdirSync(__dirname+`/commands/${dirs}`).filter(files => files.endsWith('.js'));
 
     for (const file of commands) {
         const command = require(`./commands/${dirs}/${file}`);
@@ -21,18 +21,18 @@ fs.readdirSync('./commands').forEach(dirs => {
     };
 });
 
-const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-const player = fs.readdirSync('./player').filter(file => file.endsWith('.js'));
+const events = fs.readdirSync(__dirname+'/events').filter(file => file.endsWith('.js'));
+const player = fs.readdirSync(__dirname+'/player').filter(file => file.endsWith('.js'));
 
 for (const file of events) {
     console.log(`Loading discord.js event ${file}`);
-    const event = require(`./events/${file}`);
+    const event = require(__dirname+`/events/${file}`);
     client.on(file.split(".")[0], event.bind(null, client));
 };
 
 for (const file of player) {
     console.log(`Loading discord-player event ${file}`);
-    const event = require(`./player/${file}`);
+    const event = require(__dirname+`/player/${file}`);
     client.player.on(file.split(".")[0], event.bind(null, client));
 };
 
